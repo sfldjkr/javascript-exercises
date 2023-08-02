537e74de887df05c70db07625abf4be522835310
sfldjkr <sfldjkr@gmail.com>
2023-08-02 15:18:29 +0545

    completed odinproject/js/testing/04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
 const removeFromArray = function() {
+    let argsCount = arguments.length;
+    let list = arguments[0];
+    let modifiedList = [];
+        chorus: for (let v = 0; v < list.length; v++) {
+
+                    for(let i = argsCount; i > 0; i--) {
+                        if (list[v] === arguments[i]) {
+                            continue chorus;
+                        };
+                    };
+                modifiedList.push(list[v]);
+                };
+    return modifiedList;
+
 
 };
 
