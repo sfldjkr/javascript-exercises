1c20530ff339ee08dcf99e886d815c8b06d5efcc
sfldjkr <sfldjkr@gmail.com>
2023-08-02 13:21:36 +0545

    Completed odinproject/javascript/testing/02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,n) {
+    let returnString = '';
+    if (!n | !string) {
+        return '';
+    }
 
+    else if (n < 0) {
+        return 'ERROR';
+    }
+
+    for(let i = n; i > 0; i--) {
+        returnString += `${string}`;
+    };
+    return returnString;
 };
 
 // Do not edit below this line
