66bcee06a220631990cde3d8cc7240d2c7ccc596
sfldjkr <sfldjkr@gmail.com>
2023-08-02 17:06:37 +0545

    Completed odin-projects/js/testing/05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    let sum = 0;
+    if (start < 0 || finish < 0) {
+        return 'ERROR';
+    }
+    if (typeof start == typeof finish) {
+        if (start < finish) {
+            for(let i = start; i < finish + 1; i++) {
+                sum += i;
+            };
+        }
+        else {
 
+            for(let v = start; v > finish - 1; v--) {
+                sum += v;
+                console.log(v);
+            };
+            
+        };
+        return sum;
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
