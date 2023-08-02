df6261310043f9af009164efb7c3372e82b94262
sfldjkr <sfldjkr@gmail.com>
2023-08-02 13:57:28 +0545

    Completed odinprojects/javascript/testing/03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (!string) return string;
+    // else if string is not empty
+    let stringLen = string.length - 1;
+    let returnString = "";
+    for(let i = stringLen; i > 0; i--) {
+        if (!string[i]) {
+            returnString += ' ';
+            continue;
+        }
+        returnString += `${string[i]}`;
+    }
+    returnString += `${string[0]}`;
+    return returnString;
 };
 
 // Do not edit below this line
