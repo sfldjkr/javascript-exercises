905761ad1ac58a4e39f9583bae80518b9cf0e44d
sfldjkr <sfldjkr@gmail.com>
2023-08-02 17:45:04 +0545

    Completed odinproject/js/testing/07
    took some time to tinker when n was 0

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  let answer = (n - 32) / 1.8 ;
+  if (!(n == 0)) {
+    answer = answer.toFixed(1);
+  }
+  return Number(answer);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  let answer = (n * 1.8) + 32;
+  if (!(n == 0)) {
+    answer = answer.toFixed(1);
+  }
+  console.log(answer)
+  return Number(answer);
 };
 
 // Do not edit below this line
