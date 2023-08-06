c66b48e5bdff53a4bba1d8fa787ff1f895a8110a
sfldjkr <sfldjkr@gmail.com>
2023-08-06 14:27:56 +0545

    completed odinproject/palindrome/09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
 const palindromes = function () {
+    let list = arguments[0];
+    let nonspcaedList = [];
+    for (item of list) {
+        if (item === "." || item === " " || item === "," || item === "!") {
+            continue;
+        };
+        // if (typeof +item === 'number') {
+        nonspcaedList.push(item.toUpperCase());
+        // }
+        // else {
+        //     nonspcaedList.push(item);
+        // };
+        
+    };
+    let reverseList = nonspcaedList.slice().reverse();
+
+    let answer = true;
+
+
+    for (let index = 0; index < reverseList.length; index++) {
+
+        if (reverseList[index] !== nonspcaedList[index]) {
+            return false;
+        };
+
+    };
+    return answer;
 
 };
 
-// Do not edit below this line
+
+
 module.exports = palindromes;
