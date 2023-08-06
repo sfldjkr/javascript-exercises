a57a0c73a24c1d5f926ef4f25d9221a565bee298
sfldjkr <sfldjkr@gmail.com>
2023-08-06 13:08:03 +0545

    Completed Odinproject/javascript/08
    implemented functions to make beta calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
 const add = function() {
-	
+  return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+  return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+  let list = arguments[0];
+  if (list.length === 0) return 0;
+  if (list.length === 1) return list[0];
+  let total = 0;
+  list.forEach(item => total += item);
+  return total;
+
 };
 
 const multiply = function() {
-
+  let list = arguments[0];
+  let total = 1;
+  list.forEach(item => total *= item);
+  return total;
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	let num = arguments[0];
+  // suppose the number is 5
+  if (num === 0) return 1;
+  let total = 1;
+  for (let i = 1; i < num + 1; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
